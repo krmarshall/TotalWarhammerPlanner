ec375229a10a7d7429690112e5d515d103805f7c
krmarshall <59742056+krmarshall@users.noreply.github.com>
2021-11-05 18:02:04 -0600

    Add faction icon/flag assets

---
+++ frontend/src/assets/img/flags/factionFlags.ts
@@ -0,0 +1,35 @@
+import beastmen from './beastmen/mon_256.png';
+import bretonnia from './bretonnia/mon_256.png';
+import chaos from './chaos/mon_256.png';
+import dark_elves from './dark_elves/mon_256.png';
+import dwarfs from './dwarfs/mon_256.png';
+import empire from './empire/mon_256.png';
+import greenskins from './greenskins/mon_256.png';
+import high_elves from './high_elves/mon_256.png';
+import lizardmen from './lizardmen/mon_256.png';
+import norsca from './norsca/mon_256.png';
+import skaven from './skaven/mon_256.png';
+import tomb_kings from './tomb_kings/mon_256.png';
+import vampire_coast from './vampire_coast/mon_256.png';
+import vampire_counts from './vampire_counts/mon_256.png';
+import wood_elves from './wood_elves/mon_256.png';
+
+const factionFlags = {
+  beastmen,
+  bretonnia,
+  chaos,
+  dark_elves,
+  dwarfs,
+  empire,
+  greenskins,
+  high_elves,
+  lizardmen,
+  norsca,
+  skaven,
+  tomb_kings,
+  vampire_coast,
+  vampire_counts,
+  wood_elves,
+};
+
+export default factionFlags;
