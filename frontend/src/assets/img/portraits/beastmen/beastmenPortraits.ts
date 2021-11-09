cf959e6f306f6b4d18bef7d56593b0ad06c784d0
krmarshall <59742056+krmarshall@users.noreply.github.com>
2021-11-09 16:31:02 -0700

    Start shuffling lord icons and heroes (just beastmen) into folders

---
+++ frontend/src/assets/img/portraits/beastmen/beastmenPortraits.ts
@@ -0,0 +1,27 @@
+import khazrak from './beastmen_khazrak.png';
+import malagor from './beastmen_malagor.png';
+import morghur from './wood_elves_morghur.png';
+import taurox from './wh2_dlc17_bst_taurox.png';
+import beastlord from './bst_beastlords_campaign_01_0.png';
+import greatBrayShaman from './bst_great_bray_shaman_campaign_05_0.png';
+import brayShaman from './bst_bray_shaman_campaign_03_0.png';
+import gorebull from './bst_gorebulls_campaign_01_0.png';
+import wargor from './bst_wargor_campaign_01_0.png';
+
+const beastmenPortraits = {
+  // Legendary Lords
+  khazrak,
+  malagor,
+  morghur,
+  taurox,
+  // Lords
+  beastlord,
+  greatBrayShaman,
+
+  // Heroes
+  brayShaman,
+  gorebull,
+  wargor,
+};
+
+export default beastmenPortraits;
---
+++ frontend/src/assets/img/portraits/spell_lore_icons/spellLoreIcons.ts
@@ -0,0 +1,45 @@
+import beasts from './wh_dlc03_lore_passive_wild_heart.png';
+import dark from './wh2_main_lore_passive_spiteful_conjuration.png';
+import death from './wh_main_lore_passive_life_leeching.png';
+import fire from './wh_main_lore_passive_kindleflame.png';
+import heavens from './wh_main_lore_passive_roiling_skies.png';
+import highMagic from './wh2_main_lore_passive_shield_of_saphery.png';
+import highMagicWElf from './wh2_dlc16_lore_passive_ancients_protection.png';
+import life from './wh_dlc05_spell_life_life_bloom.png';
+import light from './wh_main_lore_passive_exorcism.png';
+import metal from './wh_main_lore_passive_metalshifting.png';
+import nehekara from './wh2_dlc09_lore_nehekhara.png';
+import shadows from './wh_dlc05_lore_passive_smoke_and_mirrors.png';
+import bigWagh from './wh_main_lore_passive_power_of_da_waaagh.png';
+import deep from './wh2_dlc11_lore_deeps.png';
+import lilWagh from './wh_main_lore_passive_sneaky_stealin.png';
+import wild from './wh_dlc03_lore_passive_bestial_surge.png';
+import vampires from './wh_main_lore_passive_the_curse_of_undeath.png';
+import plague from './wh2_main_lore_passive_plague_rash.png';
+import ruin from './wh2_main_lore_passive_musk_of_fear.png';
+import stealth from './wh2_dlc14_skill_all_magic_stealth_02_toxic_rain_lore_attribute.png';
+
+const spellLoreIcons = {
+  beasts,
+  dark,
+  death,
+  fire,
+  heavens,
+  highMagic,
+  highMagicWElf,
+  life,
+  light,
+  metal,
+  nehekara,
+  shadows,
+  bigWagh,
+  deep,
+  lilWagh,
+  wild,
+  vampires,
+  plague,
+  ruin,
+  stealth,
+};
+
+export default spellLoreIcons;
