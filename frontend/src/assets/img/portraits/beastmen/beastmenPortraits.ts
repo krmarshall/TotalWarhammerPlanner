b4b6e2dc2fcb1525e9128cbf651e930999e10562
krmarshall <59742056+krmarshall@users.noreply.github.com>
2021-11-09 18:02:36 -0700

    Convert all .png to .avif

--- frontend/src/assets/img/portraits/beastmen/beastmenPortraits.ts
+++ frontend/src/assets/img/portraits/beastmen/beastmenPortraits.ts
@@ -1,12 +1,12 @@
-import khazrak from './beastmen_khazrak.png';
-import malagor from './beastmen_malagor.png';
-import morghur from './wood_elves_morghur.png';
-import taurox from './wh2_dlc17_bst_taurox.png';
-import beastlord from './bst_beastlords_campaign_01_0.png';
-import greatBrayShaman from './bst_great_bray_shaman_campaign_05_0.png';
-import brayShaman from './bst_bray_shaman_campaign_03_0.png';
-import gorebull from './bst_gorebulls_campaign_01_0.png';
-import wargor from './bst_wargor_campaign_01_0.png';
+import khazrak from './beastmen_khazrak.avif';
+import malagor from './beastmen_malagor.avif';
+import morghur from './wood_elves_morghur.avif';
+import taurox from './wh2_dlc17_bst_taurox.avif';
+import beastlord from './bst_beastlords_campaign_01_0.avif';
+import greatBrayShaman from './bst_great_bray_shaman_campaign_05_0.avif';
+import brayShaman from './bst_bray_shaman_campaign_03_0.avif';
+import gorebull from './bst_gorebulls_campaign_01_0.avif';
+import wargor from './bst_wargor_campaign_01_0.avif';
 
 const beastmenPortraits = {
   // Legendary Lords
