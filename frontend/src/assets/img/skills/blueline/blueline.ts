d3072117e3e9d0aa6fc1019374bb192d92f6be3f
krmarshall <59742056+krmarshall@users.noreply.github.com>
2021-11-12 18:18:25 -0700

    bluelineIcons export

---
+++ frontend/src/assets/img/skills/blueline/blueline.ts
@@ -0,0 +1,65 @@
+import agent from './campaign_agent.webp';
+import ambush from './campaign_ambush.webp';
+import army from './campaign_army.webp';
+import canopicJars from './campaign_canopic_jars.webp';
+import chaosCorruption from './campaign_chaos_corruption.webp';
+import disaster from './campaign_disaster.webp';
+import experience from './campaign_experience.webp';
+import fightiness from './campaign_fightiness.webp';
+import growth from './campaign_growth.webp';
+import influence from './campaign_influence.webp';
+import item from './campaign_item.webp';
+import magic from './campaign_magic.webp';
+import militarySpending from './campaign_military_spending.webp';
+import morale from './campaign_morale.webp';
+import movement from './campaign_movement.webp';
+import pirateCove from './campaign_pirate_cove.webp';
+import publicOrder from './campaign_public_order.webp';
+import replenishment from './campaign_replenishment.webp';
+import siegeAttack from './campaign_siege_attack.webp';
+import siegeDefence from './campaign_siege_defence.webp';
+import skavenCorruption from './campaign_skaven_corruption.webp';
+import slaves from './campaign_slaves.webp';
+import subterfuge from './campaign_subterfuge.webp';
+import subterranean from './campaign_subterranean.webp';
+import technology from './campaign_technology.webp';
+import trade from './campaign_trade.webp';
+import turns from './campaign_turns.webp';
+import untainted from './campaign_untainted.webp';
+import vampiricCorruption from './campaign_vampiric_corruption.webp';
+import woundTime from './campaign_wound_time.webp';
+
+const bluelineIcons = {
+  agent,
+  ambush,
+  army,
+  canopicJars,
+  chaosCorruption,
+  disaster,
+  experience,
+  fightiness,
+  growth,
+  influence,
+  item,
+  magic,
+  militarySpending,
+  morale,
+  movement,
+  pirateCove,
+  publicOrder,
+  replenishment,
+  siegeAttack,
+  siegeDefence,
+  skavenCorruption,
+  slaves,
+  subterfuge,
+  subterranean,
+  technology,
+  trade,
+  turns,
+  untainted,
+  vampiricCorruption,
+  woundTime,
+};
+
+export default bluelineIcons;
