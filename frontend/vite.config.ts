379c5612628705b5e4c8e4cba5ee2a77374b7757
krmarshall <59742056+krmarshall@users.noreply.github.com>
2022-08-15 22:12:23 -0600

    Compress built js/css with brotli and gzip to statically serve with nginx

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteCompression({ algorithm: 'brotliCompress', ext: '.br', compressionOptions: { level: 11 } }),
+    viteCompression({ algorithm: 'gzip', ext: '.gz', compressionOptions: { level: 9 } }),
+  ],
   build: {
     outDir: '../backend/public',
   },
