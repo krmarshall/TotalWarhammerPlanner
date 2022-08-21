e2f30cdd2d6340a9cf06f4062271f1e74e35c620
krmarshall <fearhorn@gmail.com>
2022-08-20 18:30:46 -0600

    Update About.tsx

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -11,14 +11,15 @@ const About = () => {
         </div>
         <p className="text-center text-gray-200 text-2xl">
           Total Warhammer Planner is a tool for exploring, building, and sharing character skill trees in Total
-          Warhammer 2/3 and various mods for both titles. Want to figure what all the various lords and heroes actually
-          do without recruiting each of them? Check what various overhaul mods have done to your favorite units? Help
-          someone new to Total Warhammer figure out how to build characters? Or maybe your just a giant nerd like me and
-          want to plan out what skills your going to have to skip. These are some of the things I wanted to address with
-          this tool and hopefully you find it useful!
+          Warhammer 2/3 and various mods.
+          <br /> Want to figure what all the various lords and heroes actually do without recruiting each of them?
+          <br /> Check what various overhaul mods have done to your favorite units?
+          <br /> Help someone new to Total Warhammer figure out how to build characters?
+          <br /> Or maybe your just a giant nerd like me and want to plan out what skills your going to have to skip.
+          <br /> These are some of the things I wanted to address with this tool and hopefully you find it useful!
         </p>
 
-        <div className="flex flex-row place-content-center mt-12">
+        {/* <div className="flex flex-row place-content-center mt-12">
           <hr className="grow mt-[1.6rem] opacity-50" />
           <h1 className="w-max text-center text-5xl mx-2 mb-2 text-gray-200 text-shadow">Patreon</h1>
           <hr className="grow mt-[1.6rem] opacity-50" />
@@ -30,7 +31,7 @@ const About = () => {
         </p>
         <a href="https://patreon.com/twplanner" target="_blank" rel="noopener noreferrer" className="mx-auto mt-2">
           <img src={patreonButton} alt="Become a Patron" className="w-auto h-12 rounded-xl" width="300" height="100" />
-        </a>
+        </a> */}
 
         <div className="flex flex-row place-content-center mt-12">
           <hr className="grow mt-[1.6rem] opacity-50" />
@@ -38,14 +39,15 @@ const About = () => {
           <hr className="grow mt-[1.6rem] opacity-50" />
         </div>
         <p className="text-center text-gray-200 text-2xl mb-8">
-          Some general items I would like to add/improve in the future, nothing is set in stone but if its listed here
-          I&apos;ve at least thought about it. Your support means I have more time to work on, and extend this list!
+          Some general items I would like to add/improve in the future, nothing is set in stone and several of these
+          would take a lot of time, but I&apos;ve at least thought about them.
         </p>
         <ul className="list-disc list-inside">
           <li className="text-gray-200 text-2xl">TW2 quest item levels</li>
           <li className="text-gray-200 text-2xl">Brettonia vow details</li>
           <li className="text-gray-200 text-2xl">More spell/ability info (damage, entities, ect)</li>
           <li className="text-gray-200 text-2xl">Mobile UI</li>
+          <li className="text-gray-200 text-2xl">Character stats</li>
         </ul>
       </div>
     </div>
