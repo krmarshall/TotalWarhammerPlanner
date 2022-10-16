ed7764e17d50c3544a6bd13a4567bd6c3de440d0
krmarshall <59742056+krmarshall@users.noreply.github.com>
2022-10-16 17:24:22 -0600

    Refactor: Change logInterval to env variable

--- backend/src/start.ts
+++ backend/src/start.ts
@@ -2,10 +2,14 @@ import app from './app';
 import { usageLog } from './usageLog';
 
 const port = process.env.PORT || 5000;
+let logInterval = 24;
+if (process.env.LOGINTERVAL !== undefined) {
+  logInterval = parseInt(process.env.LOGINTERVAL);
+}
 
 app.listen(port, () => {
   console.log(`Server is up at port ${port}`);
   console.log(`Environment ${process.env.NODE_ENV}`);
 });
 
-usageLog(8);
+usageLog(logInterval);
