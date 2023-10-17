b0aaf3473d193e684ebfab67970e5b6f40db20c8
krmarshall <59742056+krmarshall@users.noreply.github.com>
2023-10-16 22:38:44 -0600

    krmarshall/TotalWarhammerDataParser@e9f4436

--- frontend/src/pages/Issues.tsx
+++ frontend/src/pages/Issues.tsx
@@ -82,10 +82,6 @@ const Issues = () => {
             aren&apos;t visible
           </li>
           <li>Tooltips for Stat Drawer Abilities that vertically overflow the viewport are not scrollable</li>
-          <li>
-            Certain skill nodes that require x number of points in the last y skills will be missing a right arrow
-            indicating such. Most prevalent on the last skill in characters blue lines
-          </li>
           <li>Certain effects only have their first 3 abilities linked (eg. Vanilla 2 -CD to all Spells)</li>
           <IssuesSlider title="Warhammer 3" bg="bg-gray-600">
             <li>Belakor has duplicate Lord of Torment and Whispers in the Darkness skills in his tree</li>
